61263cbb9a76a30607dd8c2a6fe1858b54f60975
Ran Amir <Ranmai4@Outlook.com>
2020-07-19 05:27:14 +0300

    Schedule divider small update

--- ManageMyTime/WeeklyScheduleBar.swift
+++ ManageMyTime/WeeklyScheduleBar.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WeeklyScheduleBar: View {
     var body: some View {
          HStack{
-                                  Image(systemName: "clock").padding(EdgeInsets(top: 5, leading: 0, bottom: 3, trailing: 0)).foregroundColor(.orange)
+                                  Image(systemName: "clock").padding(EdgeInsets(top: 5, leading: 5, bottom: 3, trailing: -6)).foregroundColor(.orange)
                                   GeometryReader{geometry in
                                       Text("S").foregroundColor(Color.blue).frame(width: geometry.size.width/5.5, height:  geometry.size.height)
                                            
@@ -20,7 +20,7 @@ struct WeeklyScheduleBar: View {
                                       Text("W").foregroundColor(Color.red).frame(width: geometry.size.width/0.97, height:  geometry.size.height)
                                       Text("T").foregroundColor(Color.blue).frame(width: geometry.size.width/0.76, height:  geometry.size.height)
                                       Text("F").foregroundColor(Color.red).frame(width: geometry.size.width/0.627, height:  geometry.size.height)
-                                      Text("S").foregroundColor(Color.blue).frame(width: geometry.size.width/0.53, height:  geometry.size.height)
+                                      Text("S").foregroundColor(Color.blue).frame(width: geometry.size.width/0.54, height:  geometry.size.height)
                                   }
                    }.frame(height: 30).padding()
     }
