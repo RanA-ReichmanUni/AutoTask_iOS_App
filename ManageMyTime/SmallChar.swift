cf40cae5c12d27d1b9dfa8bd1d23895b05673d92
Ran Amir <Ranmai4@Outlook.com>
2020-08-05 23:23:25 +0300

    Git lost a file ("BigChar") not sure what is going on

--- ManageMyTime/SmallChar.swift
+++ ManageMyTime/SmallChar.swift
@@ -1,30 +1,28 @@
 //
-//  BigChar.swift
+//  SmallChar.swift
 //  ManageMyTime
 //
-//  Created by רן א on 04/08/2020.
+//  Created by רן א on 05/08/2020.
 //  Copyright © 2020 IMPACT. All rights reserved.
 //
 
 import SwiftUI
 
-struct BigChar: View {
+struct SmallChar: View {
     var hour:Int
     var body: some View {
-        HStack {
-            if(hour==Date().hour)
-            {
-                Text(String(hour)).background(RoundedRectangle(cornerRadius: 6).fill(Color.red)).foregroundColor(.white)
-                
-                 
-                 Divider().padding(EdgeInsets(top: 5, leading: -1, bottom:0, trailing: 1))
-            }
-            else{
-                Text(String(hour))
-                               
-                                
-                Divider().padding(EdgeInsets(top: 5, leading: -1, bottom:0, trailing: 1))
-            }
-              }
+            HStack {
+                            Text(String(0)).padding(EdgeInsets(top: 5, leading: -1, bottom:0, trailing: 0))
+                            
+                            Text(String(hour)).padding(EdgeInsets(top: 5, leading: -8, bottom:0, trailing:0))
+                         
+                            Divider().padding(EdgeInsets(top: 5, leading: -2, bottom:0, trailing: 0))
+                        }
+    }
+}
+
+struct SmallChar_Previews: PreviewProvider {
+    static var previews: some View {
+        SmallChar(hour:9)
     }
 }
